24fdf1e162e2b816fab72ac36e0fe91fc379549b
unclespoony <drwspoon@gmail.com>
2022-11-21 15:55:40 -0500

    Update _data.tf

--- project/solution/zone2/_data.tf
+++ project/solution/zone2/_data.tf
@@ -16,16 +16,16 @@ data "aws_caller_identity" "current" {}
    }
  }
 
-#data "terraform_remote_state" "vpc" {
-#  backend = "s3" 
-#  config = {
-#     bucket = "udacity-tf-<your_name>"
-#     key    = "terraform/terraform.tfstate"
-#     region = "us-east-2"
-#   }
+data "terraform_remote_state" "vpc" {
+  backend = "s3" 
+  config = {
+     bucket = "udacity-tf-drw"
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-2"
+   }
 
 
-#}
+}
 
 data "aws_iam_policy" "instance-policy" {
   name = "app-udacity-instance-policy"
