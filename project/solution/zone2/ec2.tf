b03430b064731dde50eb18325e590f27de965b30
unclespoony <drwspoon@gmail.com>
2022-11-22 18:25:45 -0500

    Update ec2.tf

--- project/solution/zone2/ec2.tf
+++ project/solution/zone2/ec2.tf
@@ -12,7 +12,7 @@
    instance_count     = var.instance_count
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0968aaa3c646a88eb"
+   aws_ami            = "ami-0bbdcd2a5939fb800"
    private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
    public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
    vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
