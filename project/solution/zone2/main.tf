f629140ba9fcb53a8ca2b18eba126036edd5e663
unclespoony <drwspoon@gmail.com>
2022-11-27 13:24:41 -0500

    Update main.tf

--- project/solution/zone2/main.tf
+++ project/solution/zone2/main.tf
@@ -9,22 +9,22 @@ locals {
    }
  }
       
-    module "vpc" {
-    source     = "./modules/vpc"
-    cidr_block = "10.100.0.0/16"
+    #module "vpc" {
+    #source     = "./modules/vpc"
+    #cidr_block = "10.100.0.0/16"
 
-    account_owner = local.name
-    name          = "${local.name}-project"
-    azs           = ["us-east-2a", "us-east-2b", "us-east-2c"]
-    private_subnet_tags = {
-      "kubernetes.io/role/internal-elb" = 1
-    }
-    public_subnet_tags = {
-      "kubernetes.io/role/elb" = 1
-    }
-  }
+    #account_owner = local.name
+    #name          = "${local.name}-project"
+    #azs           = ["us-east-2a", "us-east-2b", "us-east-2c"]
+    #private_subnet_tags = {
+    #  "kubernetes.io/role/internal-elb" = 1
+    #}
+    #public_subnet_tags = {
+    #  "kubernetes.io/role/elb" = 1
+    #}
+  #}
 
-   module "vpc_west" {
+   module "vpc" {
     source     = "./modules/vpc"
     cidr_block = "10.100.0.0/16"
 
