061a336fdf2fa7bbc3673f841d9ff941539bab10
unclespoony <drwspoon@gmail.com>
2022-11-27 14:07:58 -0500

    Update main.tf

--- project/solution/zone2/main.tf
+++ project/solution/zone2/main.tf
@@ -37,9 +37,9 @@ locals {
     public_subnet_tags = {
       "kubernetes.io/role/elb" = 1
     }
-   providers = {
-     aws = aws.usw1
-   }
+   #providers = {
+   #  aws = aws.usw1
+   #}
   }    
       
       
