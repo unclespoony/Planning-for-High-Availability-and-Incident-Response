7803437f4558eda5a0dc93eff2ee341288e10873
unclespoony <drwspoon@gmail.com>
2022-12-05 09:58:33 -0500

    Update rds.tf

--- project/solution/zone1/modules/rds-p/rds.tf
+++ project/solution/zone1/modules/rds-p/rds.tf
@@ -35,7 +35,7 @@ resource "aws_db_subnet_group" "udacity_db_subnet_group" {
 }
 resource "aws_rds_cluster" "udacity_cluster" {
   cluster_identifier       = "udacity-db-cluster"
-  availability_zones       = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  availability_zones       = ["us-east-2a", "us-east-2b"]
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg.name
   database_name            = "udacityc2"
   master_username          = "udacity"
