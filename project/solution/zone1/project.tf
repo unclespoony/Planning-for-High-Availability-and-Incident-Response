c9ef3ef0b74f9173b46062bca236c04224c62952
unclespoony <drwspoon@gmail.com>
2022-12-05 17:10:11 -0500

    Update project.tf

--- project/solution/zone1/project.tf
+++ project/solution/zone1/project.tf
@@ -7,29 +7,29 @@ resource "kubernetes_namespace" "udacity" {
    ]
  }
 
-#  resource "kubernetes_service" "grafana-external" {
-#  metadata {
-#    name      = "grafana-external"
-#    namespace = "monitoring"
-#    annotations = {
-#      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-#      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-#    }
-#  }
-#  spec {
-#    selector = {
-#      "app.kubernetes.io/name"="grafana"
-#    }
+  resource "kubernetes_service" "grafana-external" {
+  metadata {
+    name      = "grafana-external"
+    namespace = "monitoring"
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+    }
+  }
+  spec {
+    selector = {
+      "app.kubernetes.io/name"="grafana"
+    }
 
-#    port {
-#      port        = 80
-#      target_port = 3000
-#    }
+    port {
+      port        = 80
+      target_port = 3000
+    }
 
- #   type = "LoadBalancer"
- # }
+    type = "LoadBalancer"
+  }
 
- # depends_on = [
-#    module.project_eks
-#  ]
-#}
+  depends_on = [
+    module.project_eks
+  ]
+}
